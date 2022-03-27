202b27778116fd324dd3e6436f9f84d28b73c81c
progmamun <mamun333089@gmail.com>
2022-03-27 17:50:35 +0600

    css

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ const main = () => {
   return (
     <div>
       <div className="row justify-content-center">
-        <div className="col-md-5">
+        <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
           <h1>React TODO List</h1>
           <input
             className="form-control"
